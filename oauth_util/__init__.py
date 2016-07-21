688ffe46ab822ea4367318ec5d74a1edb2bad6d1
Alex Chan <alex@twilio.com>
2016-07-20 19:02:04 -0500

    Update imports to match oauth2client 2 packages.

--- oauth_util/__init__.py
+++ oauth_util/__init__.py
@@ -2,8 +2,8 @@ import httplib2
 
 APPENGINE = False
 try:
-  from oauth2client.appengine import CredentialsModel
-  from oauth2client.appengine import StorageByKeyName
+  from oauth2client.contrib.appengine import CredentialsModel
+  from oauth2client.contrib.appengine import StorageByKeyName
   APPENGINE = True
 except ImportError:
   pass
@@ -19,18 +19,18 @@ class Credentials(object):
     self.scope = scope
     self.secrets_file_name = secrets_file_name
     if cache_file_name is None:
-        if name is None:
-            self.cache_file_name = 'credentials_cache.json'
-        else:
-            self.cache_file_name = '%s_credentials_cache.json' % name
+      if name is None:
+        self.cache_file_name = 'credentials_cache.json'
+      else:
+        self.cache_file_name = '%s_credentials_cache.json' % name
     else:
-        self.cache_file_name = cache_file_name
+      self.cache_file_name = cache_file_name
     self.argparser = argparser
 
   def get(self):
     """Fetch, authorize, refresh token."""
     def get_file_storage():
-        return Storage(self.cache_file_name)
+      return Storage(self.cache_file_name)
     def get_storage():
       if APPENGINE:
         return StorageByKeyName(CredentialsModel, self.name, 'credentials')
@@ -48,7 +48,7 @@ class Credentials(object):
         flow = flow_from_clientsecrets(filename=self.secrets_file_name,scope=self.scope)
         from oauth2client import tools
         if self.argparser is None:
-            self.argparser = tools.argparser
+          self.argparser = tools.argparser
         credentials = tools.run_flow(flow=flow, storage=storage, flags=self.argparser.parse_args())
     else:
       credentials.refresh(httplib2.Http())
