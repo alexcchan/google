eb019439235da81f2f48591d849f94e362cdee7b
Alex Chan <alex@twilio.com>
2016-07-28 02:15:25 -0500

    Move all current modules into a parent google module.

--- google/glanguage/__init__.py
+++ google/glanguage/__init__.py
@@ -6,11 +6,22 @@ except:
 
 
 from googleapiclient import discovery
+from googleapiclient.errors import HttpError
 from oauth2client.client import GoogleCredentials
 
 
 OAUTH_SCOPE = 'https://www.googleapis.com/auth/cloud-platform'
 DISCOVERY_URL = 'https://{api}.googleapis.com/$discovery/rest?version={apiVersion}'
+
+
+class LanguageError(Exception):
+
+    def __init__(self,msg,error_code=None):
+        self.msg = msg
+        self.error_code = error_code
+
+    def __str__(self):
+        return repr('%s: %s' % (self.error_code, self.msg))
 
 
 class LanguageClient(object):
@@ -25,12 +36,18 @@ class LanguageClient(object):
         """Get named entites in document."""
         request_body={'document':{'type':'PLAIN_TEXT','content':document.encode('utf-8')},'encodingType':'UTF8'}
         service_request = self.service.documents().analyzeEntities(body=request_body)
-        response_body = service_request.execute()
+        try:
+            response_body = service_request.execute()
+        except HttpError as e:
+            raise LanguageError(e._get_reason().strip(),e.resp.status)
         return response_body['entities']
 
     def get_sentiment(self,document):
         """Get sentiment in document as polarity and magnitude."""
         request_body={'document':{'type':'PLAIN_TEXT','content':document.encode('utf-8')}}
         service_request = self.service.documents().analyzeSentiment(body=request_body)
-        response_body = service_request.execute()
+        try:
+            response_body = service_request.execute()
+        except HttpError as e:
+            raise LanguageError(e._get_reason().strip(),e.resp.status)
         return response_body['documentSentiment']
